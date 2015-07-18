03037812c0428aec74284eb335d4284ecfc74794
Stelios Omirou <selemis@gmail.com>
2015-07-18 09:47:46 +0300

    extracted transaction spec helper methods

--- spec/features/make_transaction_spec.rb
+++ spec/features/make_transaction_spec.rb
@@ -1,115 +1,110 @@
 require 'accounting_transaction'
+require 'spec_helper'
+
+include TransactionAssertable
+
 describe 'placing an accounting transaction' do
   
   before do
-      Account.delete_all
+    Account.delete_all
   end
 
   context 'salary payment with cash' do
 
+    before do
+      @salary = Account.new(name: 'Salary', category: 'Revenue')
+      @cash = Account.new(name: 'Cash', category: 'Assets')
+
+      @transaction = AccountingTransaction.new
+      @transaction.credit(@salary, 100)
+      @transaction.debit(@cash, 100)
+    end  
+
     it 'credits the salary account with an amount and debits the cash account with the same amount' do
       #TODO remember booking date
-       
-      salary = Account.new(name: 'Salary', category: 'Revenue')
-      cash = Account.new(name: 'Cash', category: 'Assets')
+      @transaction.place
 
-      transaction = AccountingTransaction.new
-      transaction.credit(salary, 100)
-      transaction.debit(cash, 100)
-      transaction.place
+      assert_transaction_placement({
+           credit_account: @salary, 
+           debit_account: @cash, 
+           credit: 100, 
+           debit: 100
+      })
+      expect(@transaction.valid?).to be_true
+    end
 
-      expect(salary.accounting_entries.size).to eq 1
-      expect(cash.accounting_entries.size).to eq 1
-      expect(salary.credit).to eq 100
-      expect(cash.debit).to eq 100
-      expect(transaction.valid?).to be_true
+    it 'commiting the transaction saves the accounting entries to the database' do
+      @transaction.place
+      @transaction.commit
 
-      transaction.commit
-
-      cash_db = Account.find_by(name: 'Cash')
-      expect(cash_db.name).to eq 'Cash'
-      expect(cash_db.accounting_entries.size).to eq 1
-      expect(cash_db.debit).to eq 100
-      expect(cash_db.credit).to eq 0
-
-      salary_db = Account.find_by(name: 'Salary')
-      expect(salary_db.name).to eq 'Salary'
-      expect(salary_db.accounting_entries.size).to eq 1
-      expect(salary_db.debit).to eq 0
-      expect(salary_db.credit).to eq 100
-
+      assert_account_from_db @cash
+      assert_account_from_db @salary
     end
 
   end
 
   context 'Withdraw money from bank' do
+
+    before do
+      @cash = Account.new(name: 'Cash', category: 'Assets')
+      @savings_account = Account.new(name: 'Savings Account', category: 'Assets')
+
+      @transaction = AccountingTransaction.new
+      @transaction.credit(@savings_account, 100)
+      @transaction.debit(@cash, 100)
+    end
     
     it 'debits the salary account with an amount and credits the savings account with the same amount' do
+      @transaction.place
 
-      cash = Account.new(name: 'Cash', category: 'Assets')
-      savings_account = Account.new(name: 'Savings Account', category: 'Assets')
+      assert_transaction_placement({
+           credit_account: @savings_account, 
+           debit_account: @cash, 
+           credit: -100, 
+           debit: 100
+      })
+      expect(@transaction.valid?).to be_true
+    end
 
-      transaction = AccountingTransaction.new
-      transaction.credit(savings_account, 100)
-      transaction.debit(cash, 100)
-      transaction.place
+    it 'commiting the transaction saves the accounting entries to the database' do
+      @transaction.place
+      @transaction.commit
 
-      expect(savings_account.accounting_entries.size).to eq 1
-      expect(cash.accounting_entries.size).to eq 1
-      expect(cash.debit).to eq 100
-      expect(savings_account.credit).to eq -100
-      expect(transaction.valid?).to be_true
-
-      transaction.commit
-
-      cash_db = Account.find_by(name: 'Cash')
-      expect(cash_db.name).to eq 'Cash'
-      expect(cash_db.accounting_entries.size).to eq 1
-      expect(cash_db.debit).to eq 100
-      expect(cash_db.credit).to eq 0
-
-      savings_account_db = Account.find_by(name: 'Savings Account')
-      expect(savings_account_db.name).to eq 'Savings Account'
-      expect(savings_account_db.accounting_entries.size).to eq 1
-      expect(savings_account_db.debit).to eq 0
-      expect(savings_account_db.credit).to eq -100
-
+      assert_account_from_db @cash
+      assert_account_from_db @savings_account
     end
   
   end
 
   context 'Pay for food with cash' do
 
+    before do
+      @cash = Account.new(name: 'Cash', category: 'Assets')
+      @groceries = Account.new(name: 'Groceries', category: 'Expenses')
+
+      @transaction = AccountingTransaction.new
+      @transaction.credit(@cash, 50)
+      @transaction.debit(@groceries, 50)
+    end
+
+
     it 'credits the cash account and debits the groceries account with the same amount' do
-      cash = Account.new(name: 'Cash', category: 'Assets')
-      groceries = Account.new(name: 'Groceries', category: 'Expenses')
+      @transaction.place
 
-      transaction = AccountingTransaction.new
-      transaction.credit(cash, 50)
-      transaction.debit(groceries, 50)
-      transaction.place
+      assert_transaction_placement({
+           credit_account: @cash, 
+           debit_account: @groceries, 
+           credit: -50, 
+           debit: 50
+      })
+    end
 
-      expect(groceries.accounting_entries.size).to eq 1
-      expect(cash.accounting_entries.size).to eq 1
-      expect(groceries.debit).to eq 50
-      expect(cash.credit).to eq -50
-      expect(transaction.valid?).to be_true
+    it 'commiting the transaction saves the accounting entries to the database' do
+      @transaction.place
+      @transaction.commit
 
-      transaction.commit
-
-      puts Account.all.size
-
-      cash_db = Account.find_by(name: 'Cash')
-      expect(cash_db.name).to eq 'Cash'
-      expect(cash_db.accounting_entries.size).to eq 1
-      expect(cash_db.debit).to eq 0
-      expect(cash_db.credit).to eq -50
-
-      groceries_db = Account.find_by(name: 'Groceries')
-      expect(groceries_db.name).to eq 'Groceries'
-      expect(groceries_db.accounting_entries.size).to eq 1
-      expect(groceries_db.debit).to eq 50
-      expect(groceries_db.credit).to eq 0
+      assert_account_from_db @cash
+      assert_account_from_db @groceries
     end
 
   end
