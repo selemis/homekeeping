b069952db84df046d228f47fecadf8a13271694b
Stelios Omirou <selemis@gmail.com>
2015-07-20 17:21:45 +0300

    commented a failing test for checking later

--- spec/models/accounting_transaction_spec.rb
+++ spec/models/accounting_transaction_spec.rb
@@ -74,7 +74,33 @@ describe AccountingTransaction do
       expect(trans.errors[:book_date].any?).to eq(true)
     end
     
-    it 'requires the credits to be equal to debits'
+    xit 'requires the credits to be equal to debits' do
+
+      cash = Account.new(name: 'Cash', category: 'Assets')
+      savings_account = Account.new(name: 'Savings Account', category: 'Assets')
+      transaction = AccountingTransaction.new
+      transaction.book_date = Date.today
+      credit_entry = AccountingEntry.new(book_date: Date.today, amount: -100)
+      debit_entry = AccountingEntry.new(book_date: Date.today, amount: 100)
+
+      credit_entry.account = savings_account
+      debit_entry.account = cash
+
+      transaction.accounting_entries << credit_entry
+      transaction.accounting_entries << debit_entry
+
+      credits = transaction.accounting_entries.map {|entry| entry.account.credit}.reduce(:+)
+      puts transaction.accounting_entries.map {|entry| entry.account.accounting_entries.size}
+      debits = transaction.accounting_entries.map {|entry| entry.account.debit}.reduce(:+)
+
+      expect(credits).to eq 100
+      expect(debits).to eq 100
+
+      
+     # transaction.credit(savings_account, 100)
+     # transaction.debit(cash, 100)
+    
+    end
 
     it 'the transaction book date must be the same as the booking dates of all its entries'
   
