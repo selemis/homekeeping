9d13624744944f8b6ab5fdd00b673d97f645c473
Stelios Omirou <selemis@gmail.com>
2015-08-03 16:57:48 +0300

    deleted commeted code

--- lib/use_cases/pay_expense.rb
+++ lib/use_cases/pay_expense.rb
@@ -4,35 +4,11 @@ require 'use_cases/make_accounting_transaction'
 
 class PayExpense < MakeAccountingTransaction
 
-  #include ActiveModel::Validations
-
-  #attr_accessor :from, :to, :date, :amount
-  #attr_reader :transaction
-
-  #validates_presence_of :date, :from, :to, :amount
   validates :from, asset_or_liabilities: true
   validates :to, expenses: true
-
-  #def initialize
-    #yield self if block_given?
-  #end
-
-  #def save
-    #if valid?
-      #@transaction = AccountingTransaction.new
-      #@transaction.book_date = date
-      #@transaction.credit(from, amount)
-      #@transaction.debit(to, amount)
-      #@transaction.save
-    #else
-      #raise 'The expense payment is not valid'
-    #end
-#
-  #end
 
   def exception_message
     'The expense payment is not valid'
   end
-
 
 end
