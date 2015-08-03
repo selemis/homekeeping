bd87a75b319ca239abf58f5d5825f652bd0767cf
Stelios Omirou <selemis@gmail.com>
2015-08-03 16:51:37 +0300

    refactoring: extracted common behavior in MakeAccountingTransaction

---
+++ lib/use_cases/make_accounting_transaction.rb
@@ -0,0 +1,25 @@
+class MakeAccountingTransaction
+  include ActiveModel::Validations
+
+  attr_accessor :from, :to, :date, :amount
+  attr_reader :transaction
+
+  validates_presence_of :date, :from, :to, :amount
+
+  def initialize
+    yield self if block_given?
+  end
+
+  def save
+    if valid?
+      @transaction = AccountingTransaction.new
+      @transaction.book_date = date
+      @transaction.credit(from, amount)
+      @transaction.debit(to, amount)
+      @transaction.save
+    else
+      raise exception_message
+    end
+  end
+
+end
--- lib/use_cases/pay_expense.rb
+++ lib/use_cases/pay_expense.rb
@@ -1,32 +1,38 @@
 require'validators/asset_or_liabilities_validator'
 require 'validators/expenses_validator'
+require 'use_cases/make_accounting_transaction'
 
-class PayExpense
+class PayExpense < MakeAccountingTransaction
 
-  include ActiveModel::Validations
+  #include ActiveModel::Validations
 
-  attr_accessor :from, :to, :date, :amount
-  attr_reader :transaction
+  #attr_accessor :from, :to, :date, :amount
+  #attr_reader :transaction
 
-  validates_presence_of :date, :from, :to, :amount
+  #validates_presence_of :date, :from, :to, :amount
   validates :from, asset_or_liabilities: true
   validates :to, expenses: true
 
-  def initialize
-    yield self if block_given?
+  #def initialize
+    #yield self if block_given?
+  #end
+
+  #def save
+    #if valid?
+      #@transaction = AccountingTransaction.new
+      #@transaction.book_date = date
+      #@transaction.credit(from, amount)
+      #@transaction.debit(to, amount)
+      #@transaction.save
+    #else
+      #raise 'The expense payment is not valid'
+    #end
+#
+  #end
+
+  def exception_message
+    'The expense payment is not valid'
   end
 
-  def save
-    if valid?
-      @transaction = AccountingTransaction.new
-      @transaction.book_date = date
-      @transaction.credit(from, amount)
-      @transaction.debit(to, amount)
-      @transaction.save
-    else
-      raise 'The expense payment is not valid'
-    end
-
-  end
 
 end
