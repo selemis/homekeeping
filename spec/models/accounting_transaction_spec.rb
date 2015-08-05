83d6f4dae6734a802c4786934f78de24c01b6aa9
Stelios Omirou <selemis@gmail.com>
2015-08-06 00:04:34 +0300

    specs refactoring

--- spec/models/accounting_transaction_spec.rb
+++ spec/models/accounting_transaction_spec.rb
@@ -20,83 +20,84 @@ describe AccountingTransaction do
     expect(trans.book_date).to eq Date.today
   end
 
-  context 'is valid' do
-  
-    context 'requires at least 2 entries' do
-        
+
+  context 'given a new transaction' do
+
+    before do
+      @trans = AccountingTransaction.new
+      @trans.valid?
+    end
+
+    it 'requires a booking date' do
+      expect(@trans.errors[:book_date].any?).to eq(true)
+    end
+
+    it ' when checking for validation then an error occurs because it has less than 2 accounting entries' do
+      expect(@trans.errors[:accounting_entries_size].any?).to eq(true)
+    end
+
+  end
+
+  context 'given a transaction with one accounting entry' do
+
+    before do
+      @trans = AccountingTransaction.new
+      entry = AccountingEntry.new(accounting_entry_attributes)
+      entry.account = Account.new(account_attributes)
+      @trans.accounting_entries << entry
+    end
+
+    it ' when checking for validation then an error occurs because it has less than 2 accounting entries' do
+      @trans.valid?
+
+      expect(@trans.errors[:accounting_entries_size].any?).to eq(true)
+    end
+
+    context 'given a transaction with two accounting entries' do
+
       before do
-        @trans = AccountingTransaction.new
+        entry = AccountingEntry.new(accounting_entry_attributes({amount: accounting_entry_attributes[:amount] * -1}))
+        entry.account = Account.new(account_attributes)
+        @trans.accounting_entries << entry
       end
 
-      it 'is not valid if it has no entries' do
+      it ' when checking validation, then it raises no errors for accounting entries size' do
         @trans.valid?
-  
-        expect(@trans.errors[:accounting_entries_size].any?).to eq(true)
-      end
-    
-      context 'it has one accounting entry' do
 
-        before do
-          entry = AccountingEntry.new(accounting_entry_attributes)
-          entry.account = Account.new(account_attributes)
-          @trans.accounting_entries << entry
-        end
-
-        it 'is not valid if it has one entry' do
-          @trans.valid?
-    
-          expect(@trans.errors[:accounting_entries_size].any?).to eq(true)
-        end
-
-        context 'it has two accounting entries' do
-          
-          before do
-            entry = AccountingEntry.new(accounting_entry_attributes({amount: accounting_entry_attributes[:amount] * -1}))
-            entry.account = Account.new(account_attributes)
-            @trans.accounting_entries << entry
-          end
-
-          it 'is valid if it has two accounting entries' do
-            @trans.valid?
-
-            expect(@trans.errors[:accounting_entries_size].any?).to eq(false)
-          end
-
-        end
-
+        expect(@trans.errors[:accounting_entries_size].any?).to eq(false)
       end
 
     end
-    
-    it 'it requires a booking date' do
-      trans = AccountingTransaction.new
 
-      trans.valid?
+  end
 
-      expect(trans.errors[:book_date].any?).to eq(true)
+  context 'given a transaction with one credit entry' do
+
+    before do
+      @transaction = AccountingTransaction.new
+      @transaction.book_date = Date.today
+      savings_account = Account.new(name: 'Savings Account', category: 'Assets')
+      credit_entry = AccountingEntry.new(book_date: Date.today, amount: -100)
+      credit_entry.account = savings_account
+      @transaction.accounting_entries << credit_entry
     end
-    
-    context 'is valid if credits equals debits' do
-      
+
+    context 'and given one debit entry of the same amount' do
+
       before do
-        @transaction = AccountingTransaction.new
-        @transaction.book_date = Date.today
-        savings_account = Account.new(name: 'Savings Account', category: 'Assets')
-        credit_entry = AccountingEntry.new(book_date: Date.today, amount: -100)
-        credit_entry.account = savings_account
         @salary = Account.new(name: 'Cash', category: 'Assets')
         debit_entry = AccountingEntry.new(book_date: Date.today, amount: 100)
         debit_entry.account = @salary
-        @transaction.accounting_entries << credit_entry << debit_entry
+        @transaction.accounting_entries << debit_entry
       end
 
-      it 'requires the credits to be equal to debits' do
+      it 'when checking for validation, then it requires the credits to be equal to debits' do
         @transaction.valid?
 
         expect(@transaction.errors[:credits_debits].any?).to be_false
       end
 
-      it 'is not valid if the credits do not equal to debits' do
+      it 'when adding another entry and checking for validation then an error occurs because credits do not equal to debits' do
         entry = AccountingEntry.new(book_date: Date.today, amount: -10)
         entry.account = @salary
         @transaction.accounting_entries << entry
@@ -108,24 +109,22 @@ describe AccountingTransaction do
 
     end
 
-    it 'the transaction book date must be the same as the booking dates of all its entries' do
-      #TODO I am sure that refactoring can be done
-      #TODO rethink spec descriptions
+    context 'and given one debit entry of the same amount and different book date' do
 
-      @transaction = AccountingTransaction.new
-      @transaction.book_date = Date.today
-      savings_account = Account.new(name: 'Savings Account', category: 'Assets')
-      credit_entry = AccountingEntry.new(book_date: Date.today, amount: -100)
-      credit_entry.account = savings_account
-      @salary = Account.new(name: 'Cash', category: 'Assets')
-      debit_entry = AccountingEntry.new(book_date: Date.parse('2015-01-01'), amount: 100)
-      debit_entry.account = @salary
-      @transaction.accounting_entries << credit_entry << debit_entry
+      before do
+        @salary = Account.new(name: 'Cash', category: 'Assets')
+        debit_entry = AccountingEntry.new(book_date: Date.parse('2015-01-01'), amount: 100)
+        debit_entry.account = @salary
+        @transaction.accounting_entries << debit_entry
+      end
 
-      expect(@transaction.valid?).to be_false
-      expect(@transaction.errors[:entries_book_date].any?).to be_true
+      it 'when checking for validation then an error occurs because the booking date of all the entries are not the same' do
+        expect(@transaction.valid?).to be_false
+        expect(@transaction.errors[:entries_book_date].any?).to be_true
+      end
+
     end
-  
+
   end
 
 end
